495d8c0cef437f439e7777c047ed483b64377cc0
sri-rishi <sririshi2612@gmail.com>
2022-03-17 00:38:54 +0530

    imported icons from react-icons

---
+++ src/Assests/icons.js
@@ -0,0 +1,9 @@
+import {FaUserCircle, FaShoppingCart, FaSearch} from "react-icons/fa";
+import {AiFillHeart} from "react-icons/ai";
+import {GoSearch} from "react-icons/go";
+import {GiHamburgerMenu} from "react-icons/gi";
+import {BsCodeSlash, BsGithub, BsTwitter} from "react-icons/bs";
+import {ImLinkedin} from "react-icons/im";
+import {SiMaildotru} from "react-icons/si"
+
+export {FaUserCircle, AiFillHeart, FaShoppingCart, GoSearch, GiHamburgerMenu, FaSearch, BsCodeSlash, BsGithub, ImLinkedin, BsTwitter, SiMaildotru};
