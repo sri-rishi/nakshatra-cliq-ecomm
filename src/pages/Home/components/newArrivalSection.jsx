7fb66714afe0ffb19384d59a210e869d2a4e39ec
sri-rishi <sririshi2612@gmail.com>
2022-03-18 00:46:50 +0530

    feat: created new arrival section

--- src/pages/Home/components/newArrivalSection.jsx
+++ src/pages/Home/components/newArrivalSection.jsx
@@ -1,11 +1,13 @@
-export const newArrivalSection = () => {
+import { comingSoonCoverImage, newArrivalCoverImage } from "../../../assets/main-images"
+
+export const NewArrivalSection = () => {
     return (
         <div className="new-section flex-column justify-center align-center gap-2">
             <h3 className="md-heading">New Collections</h3>
             <div className="new-card-div flex-row justify-evenly align-center gap-2">
                 <a href="#">
                     <div className="new-div-box overlay-box">
-                        <img className="img-responsive new-div-img" src="https://raw.githubusercontent.com/sri-rishi/nakshatra-cam-ecomm/dev/assests/images/new-arrivals-cover.jpg" alt="New Arrivals"  loading="lazy" />
+                        <img className="img-responsive new-div-img" src={newArrivalCoverImage} alt="New Arrivals" loading="lazy" />
                         <div className="overlay-cont flex-row justify-center align-center">
                             <p className="text-overlay md-heading">New Arrivals</p>
                         </div>
@@ -13,7 +15,7 @@ export const newArrivalSection = () => {
                 </a>
                 <a href="#">
                     <div className="new-div-box overlay-box">
-                        <img className="img-responsive new-div-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiUGgl7exUHx7Y8Dmau2rRLzVuuSaxn43aOw&usqp=CAU" alt="Coming Soon"  loading="lazy" />
+                        <img className="img-responsive new-div-img" src={comingSoonCoverImage} alt="Coming Soon"  loading="lazy" />
                         <div className="overlay-cont flex-row justify-center align-center">
                             <p className="text-overlay md-heading">Coming Soon</p>
                         </div>
