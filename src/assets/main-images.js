5a399cc2f7ec7d5616f0db35ff27c9887bdc6c4e
sri-rishi <sririshi2612@gmail.com>
2022-03-18 00:45:42 +0530

    imported images

---
+++ src/assets/main-images.js
@@ -0,0 +1,10 @@
+import heroCoverImage from "./images/cover-image.jpg";
+import newArrivalCoverImage from "./images/new-arrivals-cover.jpg";
+import comingSoonCoverImage from './images/coming-soon-cover.jfif';
+import dslrCoverImage from "./images/dslr-cover-image.png";
+import compactCameraImage from "./images/compact-camera-cover.jpg";
+import mirrorlessCoverImage from "./images/mirrorless-cover.jpg";
+import bridgeCameraCoverImage from "./images/bridge-camera-cover.jpg";
+
+
+export {heroCoverImage, comingSoonCoverImage, newArrivalCoverImage, dslrCoverImage, compactCameraImage, mirrorlessCoverImage, bridgeCameraCoverImage};
