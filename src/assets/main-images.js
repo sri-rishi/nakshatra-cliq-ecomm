dabf63122e6c77e16c538900cdb303028d0394ae
sri-rishi <sririshi2612@gmail.com>
2022-03-19 00:53:22 +0530

    style: added brand logos

--- src/assets/main-images.js
+++ src/assets/main-images.js
@@ -5,6 +5,10 @@ import dslrCoverImage from "./images/dslr-cover-image.png";
 import compactCameraCoverImage from "./images/compact-camera-cover.jpg";
 import mirrorLessCoverImage from "./images/mirrorless-cover.jpg";
 import bridgeCameraCoverImage from "./images/bridge-camera-cover.jpg";
+import sonyLogoImage from "./images/sony-logo.jpg";
+import canonLogoImage from "./images/canon-logo-removebg-preview.png";
+import nikonLogoImage from "./images/nikon-logo-removebg-preview.png";
+import fujifilmLogoImage from "./images/Fujifilm_logo_logotype-removebg-preview.png"
 
 
-export {heroCoverImage, comingSoonCoverImage, newArrivalCoverImage, dslrCoverImage, compactCameraCoverImage, mirrorLessCoverImage, bridgeCameraCoverImage};
+export {heroCoverImage, comingSoonCoverImage, newArrivalCoverImage, dslrCoverImage, compactCameraCoverImage, mirrorLessCoverImage, bridgeCameraCoverImage, sonyLogoImage, canonLogoImage, nikonLogoImage, fujifilmLogoImage};
