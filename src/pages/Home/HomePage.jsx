3cf2872207dffd633ab1fc92e8c2543073a0913c
sri-rishi <sririshi2612@gmail.com>
2022-03-19 01:30:57 +0530

    feat: added interest category

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,9 +1,10 @@
-import { Footer } from "../../Components/Footer/Footer"
-import { Navbar } from "../../Components/Navbar/Navbar"
-import { BrandNameSection } from "./components/BrandCategory"
-import { CategorySection } from "./components/CategorySection"
-import { HeroSection } from "./components/HeroSection"
-import { NewCategorySection } from "./components/NewCategorySection"
+import { Footer } from "../../Components/Footer/Footer";
+import { Navbar } from "../../Components/Navbar/Navbar";
+import { BrandNameSection } from "./components/BrandCategory";
+import { CategorySection } from "./components/CategorySection";
+import { HeroSection } from "./components/HeroSection";
+import { InterestCategorySection } from "./components/InterestCategory";
+import { NewCategorySection } from "./components/NewCategorySection";
 
 export const HomePage = () => {
     return (
@@ -14,6 +15,7 @@ export const HomePage = () => {
                 <NewCategorySection />
                 <CategorySection />
                 <BrandNameSection />
+                <InterestCategorySection />
                 <Footer />
             </main>
         </div>
---
+++ src/pages/Home/components/InterestCategory.jsx
@@ -0,0 +1,42 @@
+import { instantInterestLogoImage, travelInterestLogoImage, weddingInterestLogoImage, wildlifeInterestLogoImage } from "../../../assets/main-images"
+
+export const InterestCategorySection = () => {
+    const brandLogoData = [
+        {
+            id: 1, 
+            imageSrc: weddingInterestLogoImage,
+            altText: "Wedding Interest Category"
+        },
+        {
+            id: 2, 
+            imageSrc: wildlifeInterestLogoImage,
+            altText: "Wildlife Interest Category"
+        },
+        {
+            id: 3, 
+            imageSrc: travelInterestLogoImage,
+            altText: "Travel Interest Category"
+        },
+        {
+            id: 4, 
+            imageSrc: instantInterestLogoImage,
+            altText: "Instant Interest Category"
+        }
+    ]
+    return(
+        <div className="flex-column justify-center align-center gap-2">
+            <h3 className="md-heading">Top Featured Brands</h3>
+            <div className="flex-row align-center justify-evenly gap-2">
+                {
+                    brandLogoData.map(({id, imageSrc, altText}) => (
+                        <a href="#" key={id}>
+                            <div className="interest-category-div">
+                                <img className="img-responsive" src={imageSrc} alt={altText} />
+                            </div>
+                        </a>
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
