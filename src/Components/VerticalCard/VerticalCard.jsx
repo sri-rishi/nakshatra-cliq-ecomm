ffdf3f7024da4011e0b1585e918225e4593f4c68
sri-rishi <sririshi2612@gmail.com>
2022-03-20 19:06:43 +0530

    refactor: refactor code

--- src/Components/VerticalCard/VerticalCard.jsx
+++ src/Components/VerticalCard/VerticalCard.jsx
@@ -1,26 +1,25 @@
+import { AiFillHeart, FaShoppingCart} from "../../assets/icons";
+import { Button } from "../index";
+
+
 export const VerticalCard = (props) => {
-    const { brandName,categoryName,imageSrc,inStock,fastDelivery,newArrival,commingSoon,interestCategory,price,ratings,description} = props
+    const { brandName,categoryName,imageSrc,inStock,fastDelivery,newArrival,commingSoon,interestCategory,price,ratings,description} = props.productItems
     return (
         <div className="card card-vl-full-img vl-card card-shadow">
             <div className="img-vl-container img-box">
-                <img className="img-responsive cart-img" src={imageSrc} alt="Canon Camera" />
+                <img className="img-responsive cart-img" src={imageSrc} alt={`${brandName} Camera`} />
             </div>
             <div className="text-container text-vl-container text-vl-p-container">
                 <div className="card-title">
                     <h5 className="xsm-heading">{brandName}</h5>
                     <p className="card-subtitle mt-8-px text-ellipsis-overflow">{description}</p>
-                    <button className="card-btn-transparent card-like-btn">
-                        <i className="fas fa-heart"></i>
-                    </button>
+                    <Button className="card-btn-transparent card-like-btn" icon={<AiFillHeart className="icon-vr-align"/>} />
                 </div>
                 <div className="card-desc">
-                    <span className="xsm-heading">&#8377; {price.discounted}</span> <span className="text-striked">&#8377; {price.original}</span>
+                    <span className="card-dis-price">&#8377; {price.discounted}</span> <span className="text-striked">&#8377; {price.original}</span> <span className="highlighted-orange">{price.discount}</span>
                 </div>
                 <div className="card-vl-cta-section align-center">
-                    <button className="btn btn-primary">
-                        <i className="fas fa-shopping-cart"></i>
-                        Add To Cart
-                    </button>
+                    <Button className="btn btn-primary" icon={<FaShoppingCart className="icon-vr-align mr-1"/>} text="Add To Cart" />
                 </div>
             </div>
         </div>
