0bd6c252f4bf64c336630775ab51ac357905dfbe
sri-rishi <sririshi2612@gmail.com>
2022-03-20 12:32:50 +0530

    update: updated data

--- src/data/brandCategoryCover.data.js
+++ src/data/brandCategoryCover.data.js
@@ -3,21 +3,25 @@ import { canonLogoImage, fujifilmLogoImage, nikonLogoImage, sonyLogoImage } from
 export const brandLogoData = [
     {
         id: 1, 
+        brandName: "Canon",
         imageSrc: canonLogoImage,
         altText: "Canon Logo"
     },
     {
         id: 2, 
+        brandName: "Nikon",
         imageSrc: nikonLogoImage,
         altText: "Nikon Logo"
     },
     {
         id: 3, 
+        brandName: "Fujifilm",
         imageSrc: fujifilmLogoImage,
         altText: "Fujifilm Logo"
     },
     {
         id: 4, 
+        brandName: "Sony",
         imageSrc: sonyLogoImage,
         altText: "Sony Logo"
     }
