a9ceed469121f93f118afab2ee5668fd9a13c5cf
sri-rishi <sririshi2612@gmail.com>
2022-03-22 02:06:00 +0530

    chore: imported files

--- src/Components/index/index.js
+++ src/Components/index/index.js
@@ -1,6 +1,7 @@
+import { Navbar } from '../Navbar/Navbar';
 import { Footer } from '../Footer/Footer';
 import { Button } from '../Buttons/Button';
-import { Navbar } from '../Navbar/Navbar';
+
 
 
 export {Navbar, Footer, Button};
