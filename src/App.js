92f62c6fb31b81742d05ccba3c9675469b77a1d9
sri-rishi <sririshi2612@gmail.com>
2022-03-22 01:14:37 +0530

    feat: added cart component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import { ProductList } from './pages/index';
+import { Cart } from './pages/index';
 
 function App() {
   return (
     <div className="App">
-     <ProductList />
+      <Cart />
     </div>
   );
 }
