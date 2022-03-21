aaa12b75dacb2026316d4f557719bbb563aab574
sri-rishi <sririshi2612@gmail.com>
2022-03-21 12:03:48 +0530

    feat: call server

---
+++ src/data/sever-request.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+export const getProductFromMockServer = async(setData) => {
+    try {
+        const response = await axios.get("/api/products");
+        setData(response?.data?.products);
+    }catch(e) {
+        console.log(e);
+    }
+}
