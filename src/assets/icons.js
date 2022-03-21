62a3ee48319d7b76711259b416d8b5498db36ac7
sri-rishi <sririshi2612@gmail.com>
2022-03-22 01:16:26 +0530

    styel: styled components

--- src/assets/icons.js
+++ src/assets/icons.js
@@ -1,9 +1,9 @@
 import {FaUserCircle, FaShoppingCart, FaSearch} from "react-icons/fa";
-import {AiFillHeart} from "react-icons/ai";
+import {AiFillHeart, AiOutlinePlus, AiOutlineMinus} from "react-icons/ai";
 import {GoSearch} from "react-icons/go";
 import {GiHamburgerMenu} from "react-icons/gi";
 import {BsCodeSlash, BsGithub, BsTwitter} from "react-icons/bs";
 import {ImLinkedin} from "react-icons/im";
-import {SiMaildotru} from "react-icons/si"
+import {SiMaildotru} from "react-icons/si";
 
-export {FaUserCircle, AiFillHeart, FaShoppingCart, GoSearch, GiHamburgerMenu, FaSearch, BsCodeSlash, BsGithub, ImLinkedin, BsTwitter, SiMaildotru};
+export {FaUserCircle, AiFillHeart, AiOutlinePlus, AiOutlineMinus, FaShoppingCart, GoSearch, GiHamburgerMenu, FaSearch, BsCodeSlash, BsGithub, ImLinkedin, BsTwitter, SiMaildotru};
