ee385f67069a7185c7bd6cab6c31a336edfedae7
sri-rishi <sririshi2612@gmail.com>
2022-03-22 02:05:38 +0530

    feat: created wishlist page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import { ProductList } from './pages/index';
+import { Wishlist } from './pages/Wishilist/Wishlist';
+
 
 function App() {
   return (
     <div className="App">
-     <ProductList />
+      <Wishlist />
     </div>
   );
 }
