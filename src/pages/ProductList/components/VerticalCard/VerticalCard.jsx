539307c4ebea28c9dad74111a5cc95c721160eda
sri-rishi <sririshi2612@gmail.com>
2022-03-23 11:30:03 +0530

    chore: updated vertical card component

--- src/pages/ProductList/components/VerticalCard/VerticalCard.jsx
+++ src/pages/ProductList/components/VerticalCard/VerticalCard.jsx
@@ -15,8 +15,11 @@ export const VerticalCard = (props) => {
                     <p className="card-subtitle mt-8-px text-ellipsis-overflow">{description}</p>
                     <Button className="card-btn-transparent card-like-btn" icon={<AiFillHeart className="icon-vr-align"/>} />
                 </div>
-                <div className="card-desc flex-row align-center gap-8-px">
-                    <span className="card-dis-price">&#8377; {price.discounted}</span> <span className="text-striked">&#8377; {price.original}</span> <span className="orange-text font-weight-6"> &#40; {price.discount}&#37; off &#41;</span>
+                <div className="card-desc flex-column gap-1">
+                    <p className="flex-row align-center gap-8-px">
+                        <span className="card-dis-price">&#8377; {price.discounted}</span> <span className="text-striked">&#8377; {price.original}</span> <span className="orange-text font-weight-6"> &#40; {price.discount}&#37; off &#41;</span>
+                    </p>
+                    <p className="flex-row rating-box">{ratings}⭐</p>
                 </div>
                 <div className="card-vl-cta-section align-center">
                     <Button className="btn btn-primary" icon={<FaShoppingCart className="icon-vr-align mr-1"/>} text="Add To Cart" />
