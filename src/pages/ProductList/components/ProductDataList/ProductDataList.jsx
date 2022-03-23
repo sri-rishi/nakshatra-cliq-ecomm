e7529cf613fae6f5c94ef485b78c1deec0881721
sri-rishi <sririshi2612@gmail.com>
2022-03-23 11:27:47 +0530

    styles: updated style of product List page

--- src/pages/ProductList/components/ProductDataList/ProductDataList.jsx
+++ src/pages/ProductList/components/ProductDataList/ProductDataList.jsx
@@ -4,7 +4,7 @@ import { useFilteredData } from "../../../../Context/filterData.context"
 export const ProductDataList = () => {
     const {filteredByFastDeliveryData} = useFilteredData();
     return (
-        <main className="main-box grid grid-3-column">
+        <main className="main-box flex-row align-center justify-evenly gap-1">
             {
                 filteredByFastDeliveryData.map((productItems) => (
                         <VerticalCard key={productItems._id} productItems={productItems}/>
