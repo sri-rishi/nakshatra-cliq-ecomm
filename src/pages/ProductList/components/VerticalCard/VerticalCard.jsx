1001850676e6689274505a9dc74ebe227054028a
sri-rishi <sririshi2612@gmail.com>
2022-03-23 13:18:49 +0530

    feat: added new functionality

--- src/pages/ProductList/components/VerticalCard/VerticalCard.jsx
+++ src/pages/ProductList/components/VerticalCard/VerticalCard.jsx
@@ -3,11 +3,12 @@ import { Button } from "../../../../Components/index/index";
 
 
 export const VerticalCard = (props) => {
-    const { brandName,categoryName,imageSrc,inStock,fastDelivery,newArrival,commingSoon,interestCategory,price,ratings,description} = props.productItems
+    const { brandName,categoryName,imageSrc,inStock,fastDelivery,newArrival,comingSoon,interestCategory,price,ratings,description} = props.productItems
     return (
-        <div className="card card-vl-full-img vl-card card-shadow">
+        <div className={`card card-vl-full-img vl-card card-shadow overlay-box`}>
             <div className="img-vl-container img-box">
                 <img className="img-responsive cart-img" src={imageSrc} alt={`${brandName} Camera`} />
+                {newArrival ? <span class="text-badge badge-square">New</span> : ""}
             </div>
             <div className="text-container text-vl-container text-vl-p-container">
                 <div className="card-title">
@@ -19,12 +20,13 @@ export const VerticalCard = (props) => {
                     <p className="flex-row align-center gap-8-px">
                         <span className="card-dis-price">&#8377; {price.discounted}</span> <span className="text-striked">&#8377; {price.original}</span> <span className="orange-text font-weight-6"> &#40; {price.discount}&#37; off &#41;</span>
                     </p>
-                    <p className="flex-row rating-box">{ratings}⭐</p>
+                    <p className="flex-row align-center rating-box gap-8-px"><span className="rating-value">{ratings}</span><img className="star-img" src="https://img.icons8.com/color/48/000000/filled-star--v1.png"/></p>
                 </div>
                 <div className="card-vl-cta-section align-center">
-                    <Button className="btn btn-primary" icon={<FaShoppingCart className="icon-vr-align mr-1"/>} text="Add To Cart" />
+                    <Button className="btn btn-primary" icon={<FaShoppingCart className="icon-vr-align mr-1"/>} text="Add To Cart" disabled={!inStock} onClick={(e) => console.log(e.target.disabled)}/>
                 </div>
             </div>
+            {!inStock && <div class="card-overlay-div"><span class="overlay-text-box">{`${comingSoon ? "Coming soon" : "Out Of Stock"}`}</span></div>}
         </div>
     )
 }
