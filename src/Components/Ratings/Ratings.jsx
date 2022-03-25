277dcfa304f93748e1bb3136e7dc159552367d2e
sri-rishi <sririshi2612@gmail.com>
2022-03-26 01:35:03 +0530

    updated indentation

--- src/Components/Ratings/Ratings.jsx
+++ src/Components/Ratings/Ratings.jsx
@@ -1,5 +1,8 @@
 export const Ratings = (props) => {
     return (
-        <p className="flex-row align-center rating-box gap-8-px"><span className="rating-value">{props.text}</span><img className="star-img" src="https://img.icons8.com/color/48/000000/filled-star--v1.png" alt="rating"/></p>
+        <p className="flex-row align-center rating-box gap-8-px">
+            <span className="rating-value">{props.text}</span>
+            <img className="star-img" src="https://img.icons8.com/color/48/000000/filled-star--v1.png" alt="rating"/>
+        </p>
     )
 }
