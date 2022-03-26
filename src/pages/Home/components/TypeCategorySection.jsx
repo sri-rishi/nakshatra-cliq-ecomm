ef8bb0a69b02e12ca05a38dfb27c17518da649e5
sri-rishi <sririshi2612@gmail.com>
2022-03-26 11:25:01 +0530

    refactor: refactored code

--- src/pages/Home/components/TypeCategorySection.jsx
+++ src/pages/Home/components/TypeCategorySection.jsx
@@ -1,19 +1,26 @@
-import { typeCategorySectionItems } from "../../../data"
+import { Link } from "react-router-dom";
+import { useData } from "../../../Context/data.context"
+import { useFilteredData } from "../../../Context/filterData.context";
 
 export const TypeCategorySection = () => {
+    const {typeCategoriesData} = useData();
+    const {filterDispatch} = useFilteredData();
     
     return (
         <div className="flex-column justify-center align-center gap-2">
             <h3 className="md-heading">Shop By Category</h3>
             <div className="category-item-box flex-row justify-center align-center">
                 {
-                    typeCategorySectionItems.map(({id, typeCategoryName, imageSrc}) => (
-                        <a href="#" key={id}>
-                            <div className="category-items grid align-center justify-center">
-                                <img className="img-responsive category-images" src={imageSrc} alt={`${typeCategoryName} Camera Category`} />
-                                <p className="m-vl-1 xsm-heading">{typeCategoryName}</p>
+                    !typeCategoriesData ? 
+                    <div>Loading... </div>
+                    : 
+                    typeCategoriesData.map(({_id, categoryName, imageSrc}) => (
+                        <Link to="/productlist" key={_id}>
+                            <div  className="category-items grid align-center justify-center" onClick={() => (filterDispatch({type: "FILTER_BY_TYPE", payload: categoryName}))}>
+                                <img className="img-responsive category-images" src={imageSrc} alt={`${categoryName} Camera Category`} />
+                                <p className="m-vl-1 xsm-heading">{categoryName}</p>
                             </div>
-                        </a>
+                        </Link>
                     ))
                 }
             </div>
