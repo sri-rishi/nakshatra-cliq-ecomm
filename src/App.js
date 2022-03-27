7330d19309ae46df491bc926db1f1d883af7ac88
sri-rishi <sririshi2612@gmail.com>
2022-03-28 02:07:19 +0530

    chore: imported private route

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import { Cart, HomePage, MockAPI, ProductList, Wishlist, ProductDetails } from './pages/index';
+import { Cart, HomePage, MockAPI, ProductList, Wishlist, ProductDetails, PrivateRoute, SignUp, Login } from './pages/index';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<HomePage />}/>
+        <Route path="/" element={<HomePage />}/>
         <Route path="/productlist" element={<ProductList />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/wishlist" element={<Wishlist />} />
         <Route path="/productlist/:productId" element={<ProductDetails />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="/mock-api" element={<MockAPI />} />
-       </Routes>
+        <Route path="/user" element={<PrivateRoute />}>
+          <Route path="/user/cart" element={<Cart />} />
+          <Route path="/user/wishlist" element={<Wishlist />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
