694ff1cec7097146b688d43c2932c9d6a851062d
sri-rishi <sririshi2612@gmail.com>
2022-03-31 23:16:21 +0530

    feat: added price value

--- src/Helper/index.js
+++ src/Helper/index.js
@@ -2,5 +2,6 @@ import {
     getFilterByNewArrivalData,
     getFilterByPriceRangeData, getSortedProductList, getFilteredByTypeData, getFilteredByBrandData, getFilteredByInterestData, getFilteredByRatingsData, getOutOfStockFilterData, getFilterByFastDeliveryData
 } from "./filter.helper";
+import { findItemInArray, totalDiscount, totalOgPrice } from "./helperFunctions";
 
-export {getFilterByNewArrivalData, getFilterByPriceRangeData, getSortedProductList, getFilteredByTypeData, getFilteredByBrandData, getFilteredByInterestData, getFilteredByRatingsData, getOutOfStockFilterData, getFilterByFastDeliveryData}
+export {getFilterByNewArrivalData, getFilterByPriceRangeData, getSortedProductList, getFilteredByTypeData, getFilteredByBrandData, getFilteredByInterestData, getFilteredByRatingsData, getOutOfStockFilterData, getFilterByFastDeliveryData, findItemInArray, totalDiscount, totalOgPrice}
--- src/pages/Cart/components/PriceSummary/PriceSummary.jsx
+++ src/pages/Cart/components/PriceSummary/PriceSummary.jsx
@@ -1,20 +1,25 @@
-import { Button } from "../../../../Components/index"
+import { Button } from "../../../../Components/index";
+import { useCart} from "../../../../Context/cart.context";
+import { totalDiscount, totalOgPrice } from "../../../../Helper";
+
 
 export const PriceSummary = () => {
+    const {cart} = useCart();
+
     return (
         <div className="flex-column price-detail-section gap-1 card-shadow">
             <h6 className="xsm-heading border-bottom">Price Details</h6>
             <div className="flex-column gap-1 border-bottom price-value">
-                <p className="flex-row align-center justify-between"><span>Price (1 item)</span> <span>&#8377;60500</span></p>
-                <p className="flex-row align-center justify-between"><span>Discount</span> <span>&#8377;-6000</span></p>
+                <p className="flex-row align-center justify-between"><span>Price (1 item)</span> <span>&#8377;{totalOgPrice(cart)}</span></p>
+                <p className="flex-row align-center justify-between"><span>Discount</span> <span>&#8377;-{totalDiscount(cart)}</span></p>
                 <p className="flex-row align-center justify-between"><span>Delivery Charges</span> <span>&#8377;899</span></p>
             </div>
             <div className="flex-row align-center justify-between border-bottom">
                 <h6 className="xsm-heading">Total Amount</h6>
-                <p>54,500</p>
+                <p>{totalOgPrice(cart) - totalDiscount(cart)}</p>
             </div>
             <div className="flex-column gap-1">
-                <p>You will save &#8377;6000 on this order</p>
+                <p>You will save &#8377;{totalDiscount(cart)} on this order</p>
                 <Button className="btn btn-primary" text="Place Order" />
             </div>
         </div>
